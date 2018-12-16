36d75a0e48c6e1536546fc4d323a0d37ceabe752
oranos1993 <35709313+oranos1993@users.noreply.github.com>
2018-12-15 20:07:30 -0500

    Add files via upload

---
+++ model/XT_nn_essay_2.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd 
+import statsmodels.api as sm
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense,LSTM,Conv2D
+from keras.optimizers import RMSprop
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+import random
+from keras.callbacks import EarlyStopping
+
+
+df = pd.read_csv('ver2.csv')
+
+df1 = df[df['essay_set']==2]
+df2 = pd.read_csv('training_set_rel3.tsv',sep = '\t',encoding = "ISO-8859-1")
+df2 = df2[df2['essay_set']==2]
+Y = df2.rater1_domain1 + df2.rater2_domain1
+X = df1[['avg_sent_len','essay_length_no_stpwrd','num_synonym']]
+model = sm.OLS(Y, X).fit()
+predictions = model.predict(X)
+model.summary()
+
+
+Y = df2.rater1_domain1 + df2.rater2_domain1
+X = df1[['avg_sent_len','num_synonym']]
+model = sm.OLS(Y, X).fit()
+predictions = model.predict(X)
+model.summary()
+
+
+
+
+# encode class values as integers
+encoder = LabelEncoder()
+encoder.fit(Y)
+encoded_Y = encoder.transform(Y)
+# convert integers to dummy variables (i.e. one hot encoded)
+Y = np_utils.to_categorical(encoded_Y)
+df1['num_synonym_ratio'] = df1['num_synonym']/df1['essay_length']
+X = df1.drop(['essay_id', 'essay_set','essay','domain1_score','num_antonym'], axis=1)
+
+#X = df1[['num_sent','avg_sent_len','essay_length','essay_length_no_stpwrd','len_wrd',
+#         'len_wrd_stpwrd','len_uniwrd','num_uniwrd','num_uniwrd_stpwrd','num_synonym']]
+X1 = X.values.astype('float32')
+scaler = MinMaxScaler(feature_range=(0, 1))
+X1 = scaler.fit_transform(X1)
+Y = (df2.rater1_domain1 + df2.rater2_domain1).values.astype(int)
+X1 = np.reshape(X1, (X1.shape[0], 1, X1.shape[1]))
+
+rand_index = random.sample(range(len(Y)), len(Y))
+X_shuffle = X1[rand_index]
+Y_shuffle = Y[rand_index]
+split_index = int(len(Y)*0.8)
+X_train = X_shuffle[:split_index,:]
+Y_train = Y_shuffle[:split_index,]
+X_test = X_shuffle[split_index:,:]
+Y_test = Y_shuffle[split_index:,]
+
+# LSTM
+model = Sequential()
+#model.add(LSTM(64, input_shape=(1, 10)))
+model.add(LSTM(64, input_shape=(1, 71)))
+model.add(Dense(256,activation='relu'))
+model.add(Dense(64,activation='relu'))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer=RMSprop())
+model.fit(X_train, Y_train, epochs=50, batch_size=8, verbose=2,
+          validation_data=[X_test, Y_test])
+y_pred = model.predict(X_test)
+y_pred = [round(float(i)) for i in y_pred]
+sum(1 for x,y in zip(y_pred,Y_test) if (x >= y-1) and (x <= y+1)) / len(Y_test)
+
+X = df1.drop(['essay_id', 'essay_set','essay','domain1_score','num_antonym'], axis=1)
+#X = df1[['num_sent','avg_sent_len','essay_length','essay_length_no_stpwrd','len_wrd',
+#         'len_wrd_stpwrd','len_uniwrd','num_uniwrd','num_uniwrd_stpwrd','num_synonym']]
+X1 = X.values.astype('float32')
+scaler = MinMaxScaler(feature_range=(0, 1))
+X1 = scaler.fit_transform(X1)
+Y = (df2.rater1_domain1 + df2.rater2_domain1).values.astype(int)
+
+rand_index = random.sample(range(len(Y)), len(Y))
+X_shuffle = X1[rand_index]
+Y_shuffle = Y[rand_index]
+split_index = int(len(Y)*0.8)
+X_train = X_shuffle[:split_index,:]
+Y_train = Y_shuffle[:split_index,]
+X_test = X_shuffle[split_index:,:]
+Y_test = Y_shuffle[split_index:,]
+
+
+# Deep ANN
+model = Sequential()
+#model.add(Dense(10, input_dim=10, kernel_initializer='normal', activation='relu'))
+model.add(Dense(10, input_dim=71, kernel_initializer='normal', activation='relu'))
+model.add(Dense(6, kernel_initializer='normal', activation='relu'))
+model.add(Dense(1, kernel_initializer='normal'))
+model.compile(loss='mean_squared_error', optimizer=RMSprop())
+model.fit(X_train, Y_train, epochs=100, batch_size=8, verbose=2,
+          callbacks=[EarlyStopping(monitor='val_loss',min_delta=0.0001)],
+          validation_data=[X_test, Y_test])
+y_pred = model.predict(X_test)
+y_pred = [round(float(i)) for i in y_pred]
+sum(1 for x,y in zip(y_pred,Y_test) if (x >= y-1) and (x <= y+1)) / len(Y_test)
+
+
+# LSTM on Classification 
+Y = np_utils.to_categorical(encoded_Y)
+X = df1.drop(['essay_id', 'essay_set','essay','domain1_score','num_antonym'], axis=1)
+
+#X = df1[['num_sent','avg_sent_len','essay_length','essay_length_no_stpwrd','len_wrd',
+#         'len_wrd_stpwrd','len_uniwrd','num_uniwrd','num_uniwrd_stpwrd','num_synonym']]
+X1 = X.values.astype('float32')
+scaler = MinMaxScaler(feature_range=(0, 1))
+X1 = scaler.fit_transform(X1)
+X1 = np.reshape(X1, (X1.shape[0], 1, X1.shape[1]))
+rand_index = random.sample(range(len(Y)), len(Y))
+X_shuffle = X1[rand_index]
+Y_shuffle = Y[rand_index]
+split_index = int(len(Y)*0.8)
+X_train = X_shuffle[:split_index,:]
+Y_train = Y_shuffle[:split_index,]
+X_test = X_shuffle[split_index:,:]
+Y_test = Y_shuffle[split_index:,]
+
+model = Sequential()
+#model.add(LSTM(64, input_shape=(1, 10)))
+model.add(LSTM(64, input_shape=(1, 71)))
+model.add(Dense(256,activation='relu'))
+model.add(Dense(11,activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['accuracy'])
+model.fit(X_train, Y_train, epochs=20, batch_size=10, verbose=2,
+          callbacks=[EarlyStopping(monitor='val_loss',min_delta=0.0001)],
+          validation_data=[X_test, Y_test])
+y_pred = model.predict(X_train)
+
+
---
+++ model/rnn_cnn_essay4.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd 
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, LSTM, Conv1D, MaxPooling1D, Dropout, Activation,Bidirectional,Embedding
+from keras.layers.embeddings import Embedding
+from keras.optimizers import RMSprop
+from keras.callbacks import EarlyStopping
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import np_utils
+## Plotly
+import plotly.offline as py
+import plotly.graph_objs as go
+from keras.preprocessing import sequence
+from nltk.tokenize import word_tokenize
+# Others
+import nltk
+import string
+import numpy as np
+import pandas as pd
+from nltk.corpus import stopwords
+import itertools
+import random
+from sklearn.manifold import TSNE
+import string
+import sklearn
+from spellchecker import SpellChecker
+spell = SpellChecker()
+
+words = set(nltk.corpus.words.words())
+words = set([i.lower() for i in words])
+stops = set(stopwords.words("english"))
+
+df = pd.read_csv('training_set_rel3.tsv',sep = '\t',encoding = "ISO-8859-1")
+
+df = df[df['essay_set']<=6]
+
+df1 = df[df['essay_set']==4]
+
+tokenized_sents = [word_tokenize(i) for i in df1.essay.tolist()]
+for i in range(len(tokenized_sents)):
+#    tokenized_sents[i] = [w for w in tokenized_sents[i] \
+#                   if  w.lower() in words]
+    tokenized_sents[i] = [t for t in tokenized_sents[i] if t not in string.punctuation]
+    tokenized_sents[i] = [t.lower() for t in tokenized_sents[i]]
+    tokenized_sents[i] = [w for w in tokenized_sents[i] if not w in stops and len(w) >= 3]
+    
+
+combined_word = [ " ".join(i) for i in tokenized_sents]
+
+max_words = 1800
+max_len = 150
+tok = Tokenizer(num_words=max_words)
+tok.fit_on_texts(combined_word)
+sequences = tok.texts_to_sequences(combined_word)
+sequences_matrix = sequence.pad_sequences(sequences,maxlen=max_len)
+X = sequences_matrix.copy()
+
+#######added#######3
+
+
+######end############
+
+Y = df1.rater1_domain1 + df1.rater2_domain1
+encoder = LabelEncoder()
+encoder.fit(Y)
+encoded_Y = encoder.transform(Y)
+# convert integers to dummy variables (i.e. one hot encoded)
+Y = np_utils.to_categorical(encoded_Y)
+
+
+#Y = np.array(df1.domain1_score)
+rand_index = random.sample(range(len(Y)), len(Y))
+X_shuffle = X[rand_index]
+Y_shuffle = Y[rand_index]
+split_index = int(len(Y)*0.8)
+X_train = X_shuffle[:split_index,:]
+Y_train = Y_shuffle[:split_index,]
+X_test = X_shuffle[split_index:,:]
+Y_test = Y_shuffle[split_index:,]
+
+
+#def RNN():
+#    inputs = Input(name='inputs',shape=[max_len])
+#    layer = Embedding(max_words,50,input_length=max_len)(inputs)
+#    layer = LSTM(64)(layer)
+#    layer = Dense(256,name='FC1')(layer)
+#    layer = Activation('relu')(layer)
+#    layer = Dropout(0.5)(layer)
+#    layer = Dense(1,name='out_layer')(layer)
+#    layer = Activation('sigmoid')(layer)
+#    model = Model(inputs=inputs,outputs=layer)
+#    return model
+
+model = Sequential()
+model.add(Embedding(1800, 64, input_length=150))
+model.add(LSTM(64))
+model.add(Dense(256,activation='relu'))
+model.add(Dense(7, activation='sigmoid'))
+model.compile(optimizer=RMSprop(),loss = 'categorical_crossentropy',metrics=['accuracy'])
+
+train_history = model.fit(X_train,Y_train,batch_size=32,epochs=40,
+          callbacks=[EarlyStopping(monitor='val_loss',min_delta=0.0001)],
+          validation_data=[X_test, Y_test])
+
+train_history.history['loss']
+train_history.history['val_loss']
+
+
+
+model = Sequential()
+model.add(Embedding(1800, 64, input_length=150))
+model.add(Conv1D(128, 5, activation='relu'))
+model.add(MaxPooling1D(5))
+model.add(Conv1D(128, 5, activation='relu'))
+model.add(MaxPooling1D(25))
+model.add(Flatten())
+model.add(Dense(256,activation='relu'))
+model.add(Dense(7, activation='sigmoid'))
+model.compile(optimizer=RMSprop(),loss = 'categorical_crossentropy',metrics=['accuracy'])
+
+train_history = model.fit(X_train,Y_train,batch_size=32,epochs=40,
+          callbacks=[EarlyStopping(monitor='val_loss',min_delta=0.0001)],
+          validation_data=[X_test, Y_test])
+
+
+
+model = Sequential()
+model.add(Embedding(1800, 64, input_length=150))
+model.add(Conv1D(128, 5, activation='relu'))
+model.add(MaxPooling1D(5))
+model.add(LSTM(64))
+model.add(Dense(256,activation='relu'))
+model.add(Dense(7, activation='sigmoid'))
+model.compile(optimizer=RMSprop(),loss = 'categorical_crossentropy',metrics=['accuracy'])
+
+train_history = model.fit(X_train,Y_train,batch_size=32,epochs=40,
+#          callbacks=[EarlyStopping(monitor='val_loss',min_delta=0.0001)],
+          validation_data=[X_test, Y_test])
+
+Y = np.array(df1.domain1_score)
+random.seed(3)
+rand_index = random.sample(range(len(Y)), len(Y))
+X_shuffle = X[rand_index]
+Y_shuffle = Y[rand_index]
+split_index = int(len(Y)*0.8)
+X_train = X_shuffle[:split_index,:]
+Y_train = Y_shuffle[:split_index,]
+X_test = X_shuffle[split_index:,:]
+Y_test = Y_shuffle[split_index:,]
+
+model = Sequential()
+model.add(Embedding(2000, 64, input_length=300))
+model.add(LSTM(64))
+model.add(Dense(256))
+model.add(Dense(1))
+model.compile(optimizer=RMSprop(),loss = 'mean_squared_error')
+
+train_history = model.fit(X_train,Y_train,batch_size=32,epochs=40,
+                          callbacks=[EarlyStopping(monitor='val_loss',min_delta=0.0001)],
+                          validation_data=[X_test, Y_test])
